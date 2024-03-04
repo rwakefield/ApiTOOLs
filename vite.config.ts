927e1f319f85c30f07c17be161da0b7ec826205c
rwakefield <russell@Russells-MacBook-Pro.local>
2024-03-04 12:49:07 -0500

    Add bootstrap
    Render assets using bootstrap and add a generic layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+    resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
   plugins: [
     RubyPlugin(),
   ],
+  server: {
+    hmr: {
+      host: "localhost",
+      protocol: "ws",
+    },
+  },
 })
